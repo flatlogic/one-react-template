fa81ad586fae8b7dd47d2fd88084937d99eb6239
Yeliazar <erik.ruzhakov@mail.ru>
2021-09-30 15:43:00 +0300

    updated dependencies

---
+++ docs/precache-manifest.613bed58f7856553f6345a62c85c3229.js
@@ -0,0 +1,398 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b776fee91770049a36969948cb0ad6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e0ecb7ca365871e30e1",
+    "url": "./tables/css/2.566e667f.chunk.css"
+  },
+  {
+    "revision": "bff1578a305be34c6578",
+    "url": "./tables/css/main.ccd3539d.chunk.css"
+  },
+  {
+    "revision": "4e0ecb7ca365871e30e1",
+    "url": "./tables/js/2.4e0ecb7c.chunk.js"
+  },
+  {
+    "revision": "bff1578a305be34c6578",
+    "url": "./tables/js/main.bff1578a.chunk.js"
+  },
+  {
+    "revision": "5fe9fcb532ea3eb281d3",
+    "url": "./tables/js/runtime~main.5fe9fcb5.js"
+  },
+  {
+    "revision": "610f2a5ad8f48b6865b65aa96ecf2096",
+    "url": "./tables/media/1.png"
+  },
+  {
+    "revision": "cd708aaac1c953ebc42aa3b58f13d921",
+    "url": "./tables/media/2.png"
+  },
+  {
+    "revision": "e2e9acc3d90c79b5903b9835c09e5638",
+    "url": "./tables/media/3.png"
+  },
+  {
+    "revision": "991dc9b0b62193a74474aa78ed9d7236",
+    "url": "./tables/media/4.png"
+  },
+  {
+    "revision": "0406308c7c726a1b1c779130462ea42d",
+    "url": "./tables/media/5.png"
+  },
+  {
+    "revision": "5f6347ea9b23ed58fc63a464dbad7c40",
+    "url": "./tables/media/Arrow 5.5f6347ea.svg"
+  },
+  {
+    "revision": "bc82bf17366d397fcfd0b019fcc9f906",
+    "url": "./tables/media/Arrow 6.bc82bf17.svg"
+  },
+  {
+    "revision": "03aeae6033d638920b71dd8cfc0e683d",
+    "url": "./tables/media/Charts_filled.03aeae60.svg"
+  },
+  {
+    "revision": "64e593d560c666494c8345ce657f6cd6",
+    "url": "./tables/media/Charts_outlined.64e593d5.svg"
+  },
+  {
+    "revision": "2d61b79647455dc1b28c2638cbe1cc09",
+    "url": "./tables/media/Core_filled.2d61b796.svg"
+  },
+  {
+    "revision": "43f7583b4e8950d8500f9a7f29c8f6f0",
+    "url": "./tables/media/Core_outlined.43f7583b.svg"
+  },
+  {
+    "revision": "53a773a408373648d03bfe07a0a617e4",
+    "url": "./tables/media/Documentation_filled.53a773a4.svg"
+  },
+  {
+    "revision": "a4d63b9b0852e0672e7a56d399e482cc",
+    "url": "./tables/media/Documentation_outlined.a4d63b9b.svg"
+  },
+  {
+    "revision": "3856032e0c2930d8168821a3e1a62705",
+    "url": "./tables/media/E-commerce_filled.3856032e.svg"
+  },
+  {
+    "revision": "b677bb7125106dbe7cc2da3a6db0abbc",
+    "url": "./tables/media/E-commerce_outlined.b677bb71.svg"
+  },
+  {
+    "revision": "0208dadb1522b2b5291ac11e64d6b74c",
+    "url": "./tables/media/Email_filled.0208dadb.svg"
+  },
+  {
+    "revision": "c345fb38c3eb0c9c7e4b2b01b5c66eb7",
+    "url": "./tables/media/Email_outlined.c345fb38.svg"
+  },
+  {
+    "revision": "686496bccb5fdba162329ef7676f64f3",
+    "url": "./tables/media/Flaticon.686496bc.svg"
+  },
+  {
+    "revision": "76ed06ab10a4112fa3bb33bbf320cb6d",
+    "url": "./tables/media/Flaticon.76ed06ab.woff"
+  },
+  {
+    "revision": "90bc8831ccc880209459e741dc3ad6e2",
+    "url": "./tables/media/Flaticon.90bc8831.ttf"
+  },
+  {
+    "revision": "96850e104a54cdeb774cf1185b088d14",
+    "url": "./tables/media/Flaticon.96850e10.eot"
+  },
+  {
+    "revision": "7b0e92b37dec19f23eb95b978db44159",
+    "url": "./tables/media/Forms_filled.7b0e92b3.svg"
+  },
+  {
+    "revision": "c936a5f179b2ed68a10097091d967456",
+    "url": "./tables/media/Forms_outlined.c936a5f1.svg"
+  },
+  {
+    "revision": "6b904d8fd639715477a2e80aff382f4b",
+    "url": "./tables/media/Grid_filled.6b904d8f.svg"
+  },
+  {
+    "revision": "63949b05bec3e62b7238a06e6829786c",
+    "url": "./tables/media/Grid_outlined.63949b05.svg"
+  },
+  {
+    "revision": "4d4d18fdb236c6a19eb13a5687ae66a6",
+    "url": "./tables/media/Logout_filled.4d4d18fd.svg"
+  },
+  {
+    "revision": "91cb89692f1ea7f4314e9c5fd6798b7e",
+    "url": "./tables/media/Logout_outlined.91cb8969.svg"
+  },
+  {
+    "revision": "579ae3df55e04fd86ee985ed513e69d3",
+    "url": "./tables/media/Maps_filled.579ae3df.svg"
+  },
+  {
+    "revision": "1f6a676b4ea5002b5b0f5d9e3d2a08ac",
+    "url": "./tables/media/Maps_outlined.1f6a676b.svg"
+  },
+  {
+    "revision": "d7de0fb9b7e0a20a6153331ba3c18840",
+    "url": "./tables/media/Package_filled.d7de0fb9.svg"
+  },
+  {
+    "revision": "a8fbbef9c6ff9302958759d9eb5eea46",
+    "url": "./tables/media/Package_outlined.a8fbbef9.svg"
+  },
+  {
+    "revision": "d4c42dec15ac276dc321ae0fe1f07e86",
+    "url": "./tables/media/Profile_filled.d4c42dec.svg"
+  },
+  {
+    "revision": "c0a824be2361561f9d003c6adf61b65c",
+    "url": "./tables/media/Profile_outlined.c0a824be.svg"
+  },
+  {
+    "revision": "b8d88b832db3a3ef45761294d789df2f",
+    "url": "./tables/media/Settings_filled.b8d88b83.svg"
+  },
+  {
+    "revision": "afa5d5f805ac2ae264c6dac68e4dc519",
+    "url": "./tables/media/Settings_outlined.afa5d5f8.svg"
+  },
+  {
+    "revision": "5e94f36eb4f2770e1ccdbfb018e438e5",
+    "url": "./tables/media/Tables_filled.5e94f36e.svg"
+  },
+  {
+    "revision": "a6f53fb7754e75729a2d49f91a6d90fc",
+    "url": "./tables/media/Tables_outlined.a6f53fb7.svg"
+  },
+  {
+    "revision": "1354476e35a1c978bf8af2dcba578c41",
+    "url": "./tables/media/Typography-dark.1354476e.svg"
+  },
+  {
+    "revision": "0e73b9df7420ce8cfea5a223520dcf87",
+    "url": "./tables/media/Typography.0e73b9df.svg"
+  },
+  {
+    "revision": "1354476e35a1c978bf8af2dcba578c41",
+    "url": "./tables/media/Typography_filled.1354476e.svg"
+  },
+  {
+    "revision": "0e73b9df7420ce8cfea5a223520dcf87",
+    "url": "./tables/media/Typography_outlined.0e73b9df.svg"
+  },
+  {
+    "revision": "0e63cf90923bfe4b41ae033cbcd91103",
+    "url": "./tables/media/Vector-1.0e63cf90.svg"
+  },
+  {
+    "revision": "7e366352c8309aee74a1b026c54b9f3a",
+    "url": "./tables/media/Vector-2.7e366352.svg"
+  },
+  {
+    "revision": "5d5d39608600455577ba253b4d1c86a3",
+    "url": "./tables/media/Vector-3.5d5d3960.svg"
+  },
+  {
+    "revision": "24e1523a543266e99c33eb1389612d1c",
+    "url": "./tables/media/Vector-4.24e1523a.svg"
+  },
+  {
+    "revision": "f6dc73f25f30df2d36d0ae1d57f4c1fa",
+    "url": "./tables/media/a3.jpg"
+  },
+  {
+    "revision": "84f014f09e2520f76be61769a1bb6440",
+    "url": "./tables/media/a5.jpg"
+  },
+  {
+    "revision": "2966ab8e577f1e16f7cdb100af95f5a5",
+    "url": "./tables/media/a6.jpg"
+  },
+  {
+    "revision": "f7f8d087b94e59e2b4f14a56ac004de2",
+    "url": "./tables/media/account.f7f8d087.svg"
+  },
+  {
+    "revision": "8af4bbb33ac8129957dc08acbb62a787",
+    "url": "./tables/media/arrow-right.8af4bbb3.svg"
+  },
+  {
+    "revision": "59e80c76a564456c9c557d0736f2bf38",
+    "url": "./tables/media/caret-active.59e80c76.svg"
+  },
+  {
+    "revision": "55985359f28dd1de94166741ad015292",
+    "url": "./tables/media/caret.55985359.svg"
+  },
+  {
+    "revision": "1414625f4172146ad8ad874504c5cd80",
+    "url": "./tables/media/dark-dashboard.1414625f.svg"
+  },
+  {
+    "revision": "b96e4579cd2c68cd5bedd725b9c97d4c",
+    "url": "./tables/media/error-page-img.b96e4579.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./tables/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "./tables/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./tables/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./tables/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./tables/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./tables/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./tables/media/glyphicons-halflings-regular.d41d8cd9.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./tables/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "f721466883998665b87923b92dea655b",
+    "url": "./tables/media/glyphicons-halflings-regular.f7214668.svg"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./tables/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "91829f1be22b74fa63a3ba35069a93ed",
+    "url": "./tables/media/light-dashboard.91829f1b.svg"
+  },
+  {
+    "revision": "c9c38975d536e237764aad987a719b6f",
+    "url": "./tables/media/light-notify.c9c38975.svg"
+  },
+  {
+    "revision": "131b7f1e91a652791f08f5ccfe702645",
+    "url": "./tables/media/line-awesome.131b7f1e.svg"
+  },
+  {
+    "revision": "3f85d8035b4ccd91d2a1808dd22b7684",
+    "url": "./tables/media/line-awesome.3f85d803.eot"
+  },
+  {
+    "revision": "452a5b42cb4819f09d35bcf6cbdb24c1",
+    "url": "./tables/media/line-awesome.452a5b42.woff2"
+  },
+  {
+    "revision": "4d42f5f0c62a8f51e876c14575354a6e",
+    "url": "./tables/media/line-awesome.4d42f5f0.ttf"
+  },
+  {
+    "revision": "8b1290595e57e1d49d95ff3fa1129ecc",
+    "url": "./tables/media/line-awesome.8b129059.woff"
+  },
+  {
+    "revision": "5e119ff3b4bf9e9672e7b3d0aac45d4e",
+    "url": "./tables/media/logo1.5e119ff3.svg"
+  },
+  {
+    "revision": "f0f095ef89834f7b951e3ee60099b111",
+    "url": "./tables/media/logout.f0f095ef.svg"
+  },
+  {
+    "revision": "49ad4d1e5daa0a26209b8e5b38e16672",
+    "url": "./tables/media/messages-filled.49ad4d1e.svg"
+  },
+  {
+    "revision": "426865134d7c68ffdcbf97faf1674d29",
+    "url": "./tables/media/messages.42686513.svg"
+  },
+  {
+    "revision": "1de68cd8885b701940471c8ce9a8aeb1",
+    "url": "./tables/media/notify.1de68cd8.svg"
+  },
+  {
+    "revision": "f7ed3ab70ec5ddbd32100e6174256b04",
+    "url": "./tables/media/orders.f7ed3ab7.svg"
+  },
+  {
+    "revision": "477f8f1912a79f85c5a06700d609c418",
+    "url": "./tables/media/search.477f8f19.svg"
+  },
+  {
+    "revision": "30925f5d804990189319dc19eeaaafec",
+    "url": "./tables/media/settings.30925f5d.svg"
+  },
+  {
+    "revision": "b419c9ef1da808d6ee0277ee74a233a8",
+    "url": "./tables/media/signinImg.b419c9ef.svg"
+  },
+  {
+    "revision": "506bfe1d6ac28fb8e06e2a405707e516",
+    "url": "./tables/media/signupImg.506bfe1d.svg"
+  },
+  {
+    "revision": "568ca3ebeabfdf362fdcf6b868367311",
+    "url": "./tables/media/smileImg.568ca3eb.svg"
+  },
+  {
+    "revision": "e619d9fbdf82b24c666b6cf84311cb87",
+    "url": "./tables/media/stocks.e619d9fb.svg"
+  },
+  {
+    "revision": "66992d2e220692e1188aa6495a2b7bd9",
+    "url": "./tables/media/stocksDown.66992d2e.svg"
+  },
+  {
+    "revision": "5e94f36eb4f2770e1ccdbfb018e438e5",
+    "url": "./tables/media/tables-dark.5e94f36e.svg"
+  },
+  {
+    "revision": "a6f53fb7754e75729a2d49f91a6d90fc",
+    "url": "./tables/media/tables.a6f53fb7.svg"
+  },
+  {
+    "revision": "9730d62b5281f0995788186257cb4979",
+    "url": "./tables/media/total-sale.9730d62b.svg"
+  },
+  {
+    "revision": "727a55c8da9b7007e81ba6d5ac1e9e00",
+    "url": "./tables/media/ui elements_filled.727a55c8.svg"
+  },
+  {
+    "revision": "e7a048334fc951026e38fa4bc8a45a7e",
+    "url": "./tables/media/ui elements_outlined.e7a04833.svg"
+  },
+  {
+    "revision": "727a55c8da9b7007e81ba6d5ac1e9e00",
+    "url": "./tables/media/ui-elements-dark.727a55c8.svg"
+  },
+  {
+    "revision": "e7a048334fc951026e38fa4bc8a45a7e",
+    "url": "./tables/media/ui-elements.e7a04833.svg"
+  },
+  {
+    "revision": "d44327acc0c682c168700cf0f6f730a0",
+    "url": "./tables/media/usersImg.d44327ac.svg"
+  },
+  {
+    "revision": "b2e371d97ab153726b88b114d337c74f",
+    "url": "./tables/media/widget-menu.b2e371d9.svg"
+  }
+]);
